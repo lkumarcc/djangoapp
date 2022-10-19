2713a093b5d44054da88f0ad9116e029ad8d9e11
Lexi.Sareii <apreston1@tulane.edu>
2022-10-19 10:35:42 -0500

    i got output!! hello django

--- hello/views.py
+++ hello/views.py
@@ -12,3 +12,11 @@ urlpatterns = [
 ]
 
 # Create your views here.
+from django.shortcuts import render
+
+
+# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, Django!")
