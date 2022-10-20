441fc53469606ccc1879aa84c74fe8eae7b663d8
Lexi.Sareii <apreston1@tulane.edu>
2022-10-20 15:35:47 -0500

    taking in input now too

--- hello/views.py
+++ hello/views.py
@@ -2,10 +2,19 @@ import re
 from django.utils.timezone import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
+from django.views.generic import ListView
 
 
-def home(request):
-    return render(request, "hello/home.html")
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
 
 def about(request):
     return render(request, "hello/about.html")
@@ -25,6 +34,16 @@ def hello_there(request, name):
         
     )
 
-
-
 print("http://127.0.0.1:8000/hello/name")
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
